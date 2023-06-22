c26f3d735e9a98c7e3d3bce5853ef4b38e385f94
Metranohm <andrewwink@gmail.com>
2023-06-22 16:33:38 -0500

    shopping list

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import LuckyN from "./LuckyN";
-import { sum } from "./utils";
-import BoxGrid from "./BoxGrid";
-function lessThan4(dice) {
-  return sum(dice) < 4;
-}
-
-function allSameValue(dice) {
-  return dice.every((v) => v === dice[0]);
-}
+import ShoppingListForm from "./ShoppingListForm";
+import ShoppingList from "./ShoppingList";
 
 function App() {
   return (
     <>
-      {/* <BoxGrid /> */}
-      <LuckyN winCheck={lessThan4} title="Roll less than 4" />
-      <LuckyN
-        winCheck={allSameValue}
-        numDice={3}
-        title="Roll the same number"
-      />
+      <ShoppingList />
     </>
   );
 }
---
+++ src/ShoppingList.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import {v4 as uuid} from "uuid";
+import ShoppingListForm from "./ShoppingListForm";
+function ShoppingList () {
+    const [items, setItems] = useState([
+        {id: uuid(), product: "milk", quantity: "2"},
+        {id: uuid(), product: "bread", quantity: "3"}
+    ]);
+    const addItem = (item) => {
+        setItems((currItems) => {
+        return [...currItems,  {...item, id: uuid()}];
+    });
+    }
+    return (
+        <div>
+            <h1>Shopping List</h1>
+            <ul>
+                {items.map((i) => (
+                <li key={i.id}>
+                    {i.product} - {i.quantity}
+                </li>
+                ))}
+            </ul>
+            <ShoppingListForm addItem={addItem}/>
+        </div>
+    );
+}
+
+export default ShoppingList;
