a869b5b12429295b9536606a5a0099af120ff306
Metranohm <andrewwink@gmail.com>
2023-07-03 13:30:19 -0500

    refactor

--- src/QuoteFetcher.jsx
+++ src/QuoteFetcher.jsx
@@ -1,14 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { set } from "react-hook-form";
 const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
 
 export default function QuoteFetcher() {
-    const [quote, setQuote] = useState(async function(){
-        const response= await fetch(RANDOM_QUOTE_URL);
-        const jsonResponse = await response.json();
-        const randomQuote = jsonResponse.quote;
-        setQuote(randomQuote);
-    });
+    const [quote, setQuote] = useState({text: "", author: ""})
+
+    // useEffect(() => {
+    //     async function getInitialQuote() {
+    //     const response= await fetch(RANDOM_QUOTE_URL);
+    //     const jsonResponse = await response.json();
+    //     const randomQuote = jsonResponse.quote;
+    //     setQuote(randomQuote);
+    // }
+    // getInitialQuote();
+    // }, []);
+
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+    
     async function fetchQuote() {
         const response= await fetch(RANDOM_QUOTE_URL);
         const jsonResponse = await response.json();
